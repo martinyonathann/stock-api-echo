52cdbcdd8f8ff0fa241f535f2481be79d8a7381a
NB_Martin <martin.y@inception.asia>
2020-10-12 16:09:50 +0700

    controller and server

---
+++ controllers/price.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/antchfx/htmlquery"
+	"github.com/labstack/echo"
+)
+
+// compay is a structure that contains the company's stock ticker from the client's HTTP request
+
+type company struct {
+	ticker string `json:"ticker" form:"ticker" query:"ticker"`
+}
+
+// GrabPrice - handler method for binding JSON bodhy and scraping for stockprice
+func GrabPrice(c echo.Context) (err error) {
+	//read the Body content
+	var bodyBytes []byte
+	if c.Request().Body != nil {
+		bodyBytes, _ = ioutil.ReadAll(c.Request().Body)
+	}
+
+	// Restore th io.ReadCloser to its original state
+	c.Request().Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+	u := new(company)
+	er := c.Bind(u) //bind the structure with the context body
+	//on no panic!
+	if er != nil {
+		panic(er)
+	}
+	// company ticker
+	ticker := u.ticker
+	// yahoo finance base URL
+	baseURL := "https://fincance.yahoo.com/quote/"
+	// price XPath
+	pricePath := "//*[@id=\"quote-header-info\"]"
+	// load HTML document by binding base url and passed in ticker
+	doc, err := htmlquery.LoadURL(baseURL + ticker)
+	// uh oh :( freaking out!!
+	if err != nil {
+		panic(err)
+	}
+
+	//HTML Node
+	context := htmlquery.FindOne(doc, pricePath)
+	//from the Node get inner text
+	price := string(htmlquery.InnerText(context))
+	return c.JSON(http.StatusOK, price)
+
+}
---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"github.com/martinyonathann/stock-api-echo/controllers"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	//Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	//CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
+
+	//Root route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+	e.POST("/price", controllers.GrabPrice)
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
